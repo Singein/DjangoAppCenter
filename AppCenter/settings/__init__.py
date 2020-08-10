d0596454a0b36eaf75922dcc7c5162bc86789658
singein <singein@outlook.com>
2020-08-10 20:21:52 +0800

    [release] 0.0.4

--- AppCenter/settings/__init__.py
+++ AppCenter/settings/__init__.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from AppCenter.settings.admin import *
-from osprofile import OSProfile
+from OSProfile import OSProfile
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
