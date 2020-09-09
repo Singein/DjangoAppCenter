487bb770188023bce858941750cba34a50123f02
singein <singein@outlook.com>
2020-09-09 14:20:42 +0800

    build(dcoker.py): 修改了docker的构建模板，添加了几行注释

--- DjangoAppCenter/settings/docker.py
+++ DjangoAppCenter/settings/docker.py
@@ -1,19 +1,21 @@
 DOCKER_FILE_TEMPLATE = """FROM python:3.6
 
-RUN mkdir /app
-
-
 
 COPY requirements.txt .
-
 RUN pip3 install -r requirements.txt
-
 RUN pip3 install uwsgi
 
+RUN mkdir /app
 COPY DjangoAppCenter.profile.json /app
+
 # 将/app指定为工作目录
 WORKDIR /app
 
 EXPOSE 6666
 
-CMD python3 -m DjangoAppCenter deploy"""
+CMD python3 -m DjangoAppCenter deploy
+
+# 使用docker build and run
+# docker build -t django.appcenter .
+# docker run --restart=always --net=host -d --name django.appcenter django.appcenter
+"""
