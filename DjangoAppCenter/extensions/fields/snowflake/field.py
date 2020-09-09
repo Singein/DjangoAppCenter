e0f3cfe70b8ba5ad6fa877cf0aa4b094d5a7d558
singein <singein@outlook.com>
2020-09-09 15:49:18 +0800

    fix(extensions.fields.snowflake.field): 修复拼写错误

--- DjangoAppCenter/extensions/fields/snowflake/field.py
+++ DjangoAppCenter/extensions/fields/snowflake/field.py
@@ -6,4 +6,4 @@ from DjangoAppCenter.extensions.fields.snowflake.core import get_snowflake_id
 class SnowFlakeField(models.BigAutoField):
     def __init__(self, *args, **kwargs):
         kwargs['default'] = get_snowflake_id
-        super(SnowflakeField, self).__init__(*args, **kwargs)
+        super(SnowFlakeField, self).__init__(*args, **kwargs)
