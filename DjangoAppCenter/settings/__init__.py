490fb443a27f5f9f3dcf4b13495dad1a9e76dffc
singein <singein@outlook.com>
2020-09-21 19:23:42 +0800

    feat(settings): 新增了media相关的配置

--- DjangoAppCenter/settings/__init__.py
+++ DjangoAppCenter/settings/__init__.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 from django.contrib import admin
+
 from DjangoAppCenter.settings.adminui import *
+from DjangoAppCenter.settings.docker import DOCKER_FILE_TEMPLATE
 from DjangoAppCenter.settings.email import *
 from DjangoAppCenter.settings.log import LOGGING
 from DjangoAppCenter.settings.options import BASE_DIR, DEFAULT_OPTIONS, OPTIONS, PROFILE_NAME
-from DjangoAppCenter.settings.docker import DOCKER_FILE_TEMPLATE
 
 # custom_settings = OPTIONS.get('custom_settings', None)  # 暂时没用到
 
@@ -32,7 +33,6 @@ SECRET_KEY = '8v9)1@nj4g+*i56y4c5bf8-ug#!$mj*(#p^o!yw%gr9$5+2+g^'
 APP_CENTER_ENVIRON = os.environ.get("APP_CENTER_ENVIRON", "DEBUG")
 DEBUG = APP_CENTER_ENVIRON == "DEBUG"
 
-
 ALLOWED_HOSTS = ["*"]
 # 跨域请求设置的基本参数
 CORS_ORIGIN_ALLOW_ALL = True
@@ -41,27 +41,27 @@ CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
-    'DjangoAppCenter.simpleui',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'django_filters'
-] + OPTIONS.get('apps', [])
+                     'DjangoAppCenter.simpleui',
+                     'django.contrib.admin',
+                     'django.contrib.auth',
+                     'django.contrib.contenttypes',
+                     'django.contrib.sessions',
+                     'django.contrib.messages',
+                     'django.contrib.staticfiles',
+                     'rest_framework',
+                     'django_filters'
+                 ] + OPTIONS.get('apps', [])
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-] + OPTIONS.get('middlewares', [])
+                 'django.middleware.security.SecurityMiddleware',
+                 'django.contrib.sessions.middleware.SessionMiddleware',
+                 'corsheaders.middleware.CorsMiddleware',
+                 'django.middleware.common.CommonMiddleware',
+                 'django.middleware.csrf.CsrfViewMiddleware',
+                 'django.contrib.auth.middleware.AuthenticationMiddleware',
+                 'django.contrib.messages.middleware.MessageMiddleware',
+                 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+             ] + OPTIONS.get('middlewares', [])
 
 ROOT_URLCONF = 'DjangoAppCenter.settings.urls'
 
@@ -84,12 +84,10 @@ TEMPLATES = [
 DATABASE_ROUTERS = ['DjangoAppCenter.settings.dbrouters.Router']
 WSGI_APPLICATION = 'DjangoAppCenter.settings.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = OPTIONS.get('databases', None)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -109,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -123,15 +120,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = OPTIONS.get('static_root')
+STATIC_URL = OPTIONS.get('static_url') if OPTIONS.get('static_url') else '/static/'
+STATIC_ROOT = OPTIONS.get('static_root') if OPTIONS.get('static_root') else os.path.join(os.getcwd(), 'statics')
+
+MEDIA_URL = OPTIONS.get('media_url') if OPTIONS.get('media_url') else '/media/'
+MEDIA_ROOT = OPTIONS.get('media_root') if OPTIONS.get('media_root') else os.path.join(os.getcwd(), 'uploads')
 
 REST_FRAMEWORK = OPTIONS.get('rest_framework', {})
-
 
 admin.AdminSite.site_title = OPTIONS.get('admin_site_title', 'DjangoAppCenter')
 admin.AdminSite.site_header = OPTIONS.get(
