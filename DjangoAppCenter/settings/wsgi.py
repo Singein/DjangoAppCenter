2ec4a848ca59291107055031090b222435d08c0c
singein <singein@outlook.com>
2020-10-05 15:38:31 +0800

    fix(wsgi): 修改了wsgi的逻辑

--- DjangoAppCenter/settings/wsgi.py
+++ DjangoAppCenter/settings/wsgi.py
@@ -7,8 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+import os
+
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoAppCenter.settings')
+from DjangoAppCenter.settings import load_settings
 
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoAppCenter.settings')
+os.environ.setdefault('APP_CENTER_ENVIRON', 'PROD')
+custom_settings = load_settings()
+settings.configure(**custom_settings)
 application = get_wsgi_application()
