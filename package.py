82cd658a9c3133f3ddba1ab769d2ca786b25db4e
singein <singein@outlook.com>
2021-03-30 13:06:32 +0800

    [release] 0.9.11

--- package.py
+++ package.py
@@ -1,11 +1,11 @@
 package = {
     'name': 'DjangoAppCenter',
-    'version': '0.9.9',
+    'version': '0.9.11',
     'author': 'singein',
     'email': 'singein@outlook.com',
     'scripts': {
         'default': 'echo 请输入明确的命令名称',
         'migrate': 'python -m DjangoAppCenter makemigrations && python -m DjangoAppCenter migrate',
-        'upload': 'twine upload --repository-url=http://0.9.9.46:5555 --trusted-host=0.9.9.46'
+        'upload': 'twine upload --repository-url=http://0.9.11.46:5555 --trusted-host=0.9.11.46'
     }
 }
